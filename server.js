2c4686c8e2bec40673eef4bd5b0498d26ceeb8f3
matthew <matthew.villegas@knights.ucf.edu>
2018-10-10 19:36:29 -0400

    added contact schema for student and professor, and connecting to mongo

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const path = require("path");
 const config = require('./dbcreds');
+const crypto = require("crypto");
 
 var app = express();
 app.use(bodyParser.json());
@@ -13,16 +14,65 @@ mongoose.connect(config.dburl);
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function(){
-	// should be connected
-	console.log('connected to remote database');
+  // should be connected
+  console.log('connected to remote database');
 });
 
-
 // app.get('*', function(req,res){
-//     res.sendFile(path.join(__dirname, 'public/index.html'));
+// res.sendFile(path.join(__dirname, 'public/index.html'));
 // });
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+var port = server.address().port;
   console.log("App now running on port", port);
+});
+
+var studentSchema = new mongoose.Schema({
+  studentNID: String,
+  name: String,
+  email: String,
+  studentUUID: String
+});
+
+var professorSchema = new mongoose.Schema({
+  profNID: String,
+  name: String,
+  hashPassword: String,
+  hasPaid: Boolean, 
+  bleUUID: String
+});
+
+var Student = mongoose.model("Student", studentSchema);
+var Professor = mongoose.model("Professor", professorSchema);
+
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+// incoming json request should be in the form of 
+// nid: "professorNidHere"
+// name: "professerName"
+// password: "password"
+// 
+app.post("/api/createProfessor", function(req,res) {
+
+  var professor = new Professor({
+    profNID: req.body.nid,
+    name: req.body.name,
+    hashPassword : crypto.createHash('sha256').update(JSON.stringify(req.body.password)).digest('hex')
+
+    })
+  if(!req.body.nid || !req.body.name || !req.body.password) {
+    handleError(res, "Invalid user input", "Missing Creation Parameter", 400);
+  } else {
+    professor.save(function(err, professor) {
+    if(err) {
+      handleError(res, "Databse error", "Error saving user data");
+    } else {
+     console.log("Professor successfully created!");
+     res.status(201).json("success");
+     }
+    });
+  }
 });
