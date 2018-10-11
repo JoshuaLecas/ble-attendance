5cf387c583cfb4999607d40246d9c5d1fdb45c93
unknown <zzraina619@gmail.com>
2018-10-10 21:02:04 -0400

    can add classes according to profNID and find what classes a professor has created according to profNID

--- server.js
+++ server.js
@@ -43,8 +43,18 @@ var professorSchema = new mongoose.Schema({
   email: String
 });
 
+var classSchema = new mongoose.Schema({
+	classID: String,
+	className: String,
+	createdByProfNID: String,
+	startTime: String,
+	endTime: String,
+	numEnrolled: Number
+});
+
 var Student = mongoose.model("Student", studentSchema);
 var Professor = mongoose.model("Professor", professorSchema);
+var Class = mongoose.model("Class", classSchema);
 
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
@@ -86,6 +96,48 @@ app.get("/api/professors", function(req, res) {
 		}
 		else {
 			res.status(201).json(professors);
+		}
+	});
+});
+
+// JSON
+// class_id: "cop4331"
+// name: "poop"
+// start_time: "13:00"
+// end_time: "14:50"
+
+app.post("/api/classes/create/:id", function(req, res) {
+	// "class" is a reserved word in js
+	var classy = new Class({
+		classID: req.body.class_id,
+		className: req.body.name,
+		createdByProfNID: req.params.id,
+		startTime: req.body.start_time,
+		endTime: req.body.end_time
+	});
+
+	if(!req.body.class_id) {
+		handleError(res, "Invalid user input", "Missing class id", 400);
+	}
+	else {
+		classy.save(function(err, classy){
+			if(err) return console.log("Didn't save");
+			else {
+				console.log("Class successfully created");
+				res.status(201).json(classy);
+			}
+		});
+	}
+
+});
+
+app.get("/api/classes/:id", function(req, res) {
+	Class.find({createdByProfNID: req.params.id}, function(err, profClasses) {
+		if(err) {
+			handleError(res, err.message, "Couldn't get classes for professor");
+		}
+		else {
+			res.status(201).json(profClasses);
 		}
 	});
 });
