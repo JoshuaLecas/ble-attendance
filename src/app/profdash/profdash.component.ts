65a74c4b81dc04f66a2d7905141e393b9d1a2d3b
Josh <jlecas@knights.ucf.edu>
2018-11-15 21:36:50 -0500

    Basic skeleton created, work needed

--- src/app/profdash/profdash.component.ts
+++ src/app/profdash/profdash.component.ts
@@ -10,7 +10,22 @@ export class ProfdashComponent implements OnInit {
 
   constructor(private router: Router) { }
 
+
+  user : Object;
+  user_id: String;
+
   ngOnInit() {
+  //  if (sessionStorage.length == 0){
+  //    this.router.navigate(['/home']);
+  //  }
+  //  this.pageLoad();
+  }
+
+  pageLoad(){
+    var temp = sessionStorage.getItem('user');
+    this.user = JSON.parse(temp);
+    this.authService.storeUser(this.user);
+    this.user_id = this.user['id'];
   }
 
 }
