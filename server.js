b72241fa318232280871a5bc1833e09ccae79989
matthew <matthew.villegas@knights.ucf.edu>
2018-12-03 22:50:01 -0500

    Added new endpoint for getting class info by id

--- server.js
+++ server.js
@@ -436,6 +436,23 @@ app.get("/api/classes/:id", function(req, res) {
 	});
 });
 
+// View Classes (by _id)
+
+// :id - class _id
+app.get("/api/classes/viewClass/:id", function(req, res) {
+
+	Class.find({_id: req.params.id}, function(err, thisClass) {
+		if(err) {
+			handleError(res, "Database error error while searching", "Failed get classes for professor");
+		}
+		else {
+			console.log("Successfully got classes");
+			res.status(201).json(thisClass);
+		}
+	});
+});
+
+
 // Delete Class
 
 // :id - "_id of class"
